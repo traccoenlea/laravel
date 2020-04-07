f9f0b02726ce2c07135bbf2f1d6550aa0436942e
Cloelh <cloelh@outlook.fr>
2020-04-07 09:08:22 +0200

    commit cloé

--- app/Http/Controllers/OrdisController.php
+++ app/Http/Controllers/OrdisController.php
@@ -5,8 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class OrdisController extends Controller
-{
+{	
+
+
     public function display(){
+
+    	
+    	
         $ordis = [1=> "Ordis 1", 2=>"Ordis 2", 3=> "Ordis 3"];
     	return view("ordis.display", ["ordis" => $ordis]);
     }
--- resources/views/ordis/comparateur_pro.blade.php
+++ resources/views/ordis/comparateur_pro.blade.php
@@ -4,7 +4,7 @@
 <div class="comparateur comparateur_pro">
 	<h2>Comparateur Professionnel</h2>
 	<p>Pour les gens qui savent de quoi ils parlent ;)</p>
-	<div class="list-item-ordi"> 
+	<div class="list-item-ordi	"> 
 		@foreach($item as $id => $nom)
 			<div class="list-group-item">{{$nom}}</div>
 		@endforeach
