3e946594b8a2b7e26f4990d54684a5eeb2d47b11
Cloelh <cloelh@outlook.fr>
2020-05-07 14:56:37 +0200

    cloe

--- app/ordinateur.php
+++ app/ordinateur.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Ordinateur extends Model
 {
     public $table = 'ordinateur';
+
+    public function vendeurs() {
+        return $this->belongsToMany('App\Vendeurs', 'achats', 'id_ordinateur', 'id_vendeur') ->withPivot('prix', 'url');
+        
+    }
 }
